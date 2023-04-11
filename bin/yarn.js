b358550db9199e4b591f880a9fa27d887711e53a
Thach Nguyen <thachnn80@gmail.com>
2023-04-11 17:48:37 +0700

    Re-pack `yarn` v1.22.19 from ESM source using `webpack` v5

--- bin/yarn.js
+++ bin/yarn.js
@@ -1,31 +1,15 @@
 #!/usr/bin/env node
+"use strict";
 
-/* eslint-disable no-var */
-/* eslint-disable flowtype/require-valid-file-annotation */
-'use strict';
+var ver = process.versions.node, majorVer = parseInt(ver.split(".")[0], 10);
 
-var ver = process.versions.node;
-var majorVer = parseInt(ver.split('.')[0], 10);
-
-if (majorVer < 4) {
-  console.error('Node version ' + ver + ' is not supported, please use Node.js 4.0 or higher.');
-  process.exit(1); // eslint-disable-line no-process-exit
-} else {
+if (majorVer < 4) console.error("Node version " + ver + " is not supported, please use Node.js 4.0 or higher."), 
+process.exit(1); else {
   try {
-    require(__dirname + '/../lib/v8-compile-cache.js');
-  } catch (err) {
-    // We don't have/need this on legacy builds and dev builds
-  }
-
-  // Just requiring this package will trigger a yarn run since the
-  // `require.main === module` check inside `cli/index.js` will always
-  // be truthy when built with webpack :(
-  // `lib/cli` may be `lib/cli/index.js` or `lib/cli.js` depending on the build.
-  var cli = require(__dirname + '/../lib/cli');
-  if (!cli.autoRun) {
-    cli.default().catch(function(error) {
-      console.error(error.stack || error.message || error);
-      process.exitCode = 1;
-    });
-  }
+    require("../lib/v8-compile-cache.js");
+  } catch (err) {}
+  var cli = require("../lib/cli");
+  cli.autoRun || cli.default().catch((function(error) {
+    console.error(error.stack || error.message || error), process.exitCode = 1;
+  }));
 }
